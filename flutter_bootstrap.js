f86a8c366685e31c2ba7de9204d805f0feaff14c
rvwmr <rvda2004@gmail.com>
2024-12-03 01:08:05 +0530

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1759830833"
+    serviceWorkerVersion: "1913594578"
   }
 });
